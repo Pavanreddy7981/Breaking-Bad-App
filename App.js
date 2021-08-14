b4b29162de790ea8dc36b254145068581410afe6
Pavanreddy7981 <pavanreddy7981@gmail.com>
2021-08-14 10:28:22 +0530

    initial-Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>This is the Breaking Bad App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
